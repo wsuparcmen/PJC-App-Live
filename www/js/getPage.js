71816205a9934d23d1661d4f2c56474c92954ed4
Home <Home>
2016-02-10 10:15:59 -0700

    delete token

--- www/js/getPage.js
+++ www/js/getPage.js
@@ -1,7 +1,15 @@
 jQuery(document).ready(function () {
     jQuery('#clickme').on('click', function () {
         jQuery('#display').text("Hello worldlings!");
+		jQuery('#token').html(window.localStorage.getItem("token"));
     });
+	
+	jQuery('#delete').on('click', function () {
+        jQuery('#token').html("");
+		window.localStorage.removeItem("token");
+		jQuery('#deletedToken').html(window.localStorage.getItem("token"));
+    });
+	
     var url = 'http://pjc.gear.host/api/Hello';
     $.ajax({
         type: 'GET',
