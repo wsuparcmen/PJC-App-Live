8b16438e5cfe866f1859a9031c0609488cf0ed2c
Livingst0n <mikelivingston04@outlook.com>
2016-02-11 16:14:59 -0700

    API to authenticate token

--- www/js/getPage.js
+++ www/js/getPage.js
@@ -5,6 +5,7 @@ jQuery(document).ready(function () {
     });
     
     var AUTH_TOKEN = window.localStorage.getItem("token");
+    var oldToken = "b33c551a";
     //var AUTH_TOKEN = "a" + window.localStorage.getItem("token");
 	
 	jQuery('#delete').on('click', function () {
@@ -13,18 +14,19 @@ jQuery(document).ready(function () {
 		jQuery('#deletedToken').text(window.localStorage.getItem("token"));
     });
 	
-    var url = 'http://pjc.gear.host/api/Hello';
+    var url = 'http://pjc.gear.host/api/AuthTest';
     $.ajax({
         type: 'GET',
-        url: url + "?authenticity_token=" + AUTH_TOKEN,
+        url: url + "?token=" + AUTH_TOKEN,
         dataType: 'json',
         success: function (data, status) {
-            $.each(data, function (key, item) {
+            jQuery("#successOrFailure").text("SUCCESS!");
+            /*$.each(data, function (key, item) {
                 $('<li>', { text: formatItem(item) }).appendTo($('#records'));
-            });
+            });*/
         },
         error: function () {
-            alert("something broke");
+            jQuery("#successOrFailure").text("FAILURE!");
         }
     });
 
