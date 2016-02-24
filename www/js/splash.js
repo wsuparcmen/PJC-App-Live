bb881cfee492df047675e0c55f8510fb9633feab
Mike Livingston <mlivingston@axis41.com>
2016-02-24 12:25:31 -0700

    Update to splash and login ajax calls

--- www/js/splash.js
+++ www/js/splash.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function () {
     var AUTH_TOKEN = window.localStorage.getItem("token");
     var oldToken = "b33c551a";
 	
-    var url = 'http://pjc.gear.host/api/AuthTest';
+    /*var url = 'http://pjcdbrebuild.gear.host/api/AuthTest';
     $.ajax({
         type: 'GET',
         url: url + "?token=" + AUTH_TOKEN,
@@ -12,9 +12,10 @@ jQuery(document).ready(function () {
             jQuery("#successOrFailure").text("SUCCESS!");
         },
         error: function () {
+            alert("boo");
             window.location.href="Login.html";
         }
-    });
+    });*/
     
     logout = function() {
         window.localStorage.removeItem("token");
