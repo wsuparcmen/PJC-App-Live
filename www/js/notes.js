caa7f155267136f11f9a8eae11633da489011b1d
Diana <dianango@mail.weber.edu>
2016-03-18 15:50:46 -0600

    added functionality to edit user notes

--- www/js/notes.js
+++ www/js/notes.js
@@ -25,16 +25,18 @@ $(document).on("pagecreate", function(){
 		}
 		else{
 			$("#previousNotes").append("<div data-role='collapsible'>" +
-											"<h3>" + name + "</h3>" +
-											"<p class='name'><strong>" + name + "</strong></p>" +
-											"<p class='content'>" + note + "</p>" +
+											"<h3><span class='title'>" + name + "</span></h3>" +
+											"<div class='content'>" +
+												"<strong class='name'>" + name + "</strong>" + 
+												"<p class='body'>" + note + "</p>" +
+											"</div>" +
 											"<div class='ui-grid-a'>" +
 												"<div class='ui-block-a'><a href='#' class='ui-btn edit'>Edit</a></div>" +
 												"<div class='ui-block-b'><a href='#' class='ui-btn delete'>Delete</a></div>" +
 											"</div>" +
-										"</div>");
-										
+										"</div>");	
 			$("#previousNotes").collapsibleset("refresh");
+			document.getElementById("noteForm").reset();
 			
 			$(".delete").on("click", function(){
 				var item = $(this).parent("div").parent("div").parent("div").parent("div");
@@ -45,6 +47,8 @@ $(document).on("pagecreate", function(){
 				var item = $(this).parent("div").parent("div").parent("div").parent("div");
 				edit(item);
 			});
+			
+			//save note
 		}
 	});
 	
@@ -62,6 +66,8 @@ function confirmAndDelete(item){
 	$("#confirm #yes").on("click", function(){
 		item.remove();
 		$("#previousNotes").collapsibleset("refresh");
+		
+		//delete note
 	});
 	
 	$("#confirm #cancel").on("click", function(){
@@ -70,5 +76,25 @@ function confirmAndDelete(item){
 }
 
 function edit(item){
+	var name = item.find(".name")[0].innerText;
+	var note = item.find(".body")[0].innerText;
 	
+	item.find(".content")[0].innerHTML = "<form class='ui-body ui-body-a'>" +
+											"<label for='noteName'>Name</label>" +
+											"<input type='text' name='noteName' id='editName' value='" + name + "'/>" +
+											"<label for='note'>Note</label>" +
+											"<textarea cols='40' name='note' id='editBody'>" + note + "</textarea>" +
+											"<a href='#' class='ui-btn' id='saveEdit'>Save</a>" +
+										"</form>";
+	
+	$("#saveEdit").click(function(){
+		name = document.getElementById("editName").value;
+		note = document.getElementById("editBody").value;
+		
+		item.find(".content")[0].innerHTML = "<strong class='name'>" + name + "</strong>" + 
+											"<p class='body'>" + note + "</p>";
+		item.find(".title")[0].innerText = name;
+		
+		//save edited note
+	});
 }
