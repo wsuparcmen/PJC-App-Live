9a53d5b23c92e492d1bf5158ba6b8c70e40d6709
Diana <dianango@mail.weber.edu>
2016-03-18 14:22:00 -0600

    updated the save and delete functionality on the user notes page

--- www/js/notes.js
+++ www/js/notes.js
@@ -11,69 +11,64 @@ account = function() {
 }
 
 $(document).on("pagecreate", function(){
-	var count = 1;
+	var count = 0;
 	$("#save").click(function(){
-		count++;
-		$("#previousNotes").append("<li>" +
-										"<a href='#'>" +
-											"<h3>" + document.getElementById("noteName").value + "</h3>" +
-											"<p class='topic'><strong>" + document.getElementById("noteName").value + "</strong></p>" +
-											"<p>" + document.getElementById("note").value + "</p>" +
-										"</a>" +
-										"<a href='#' class='delete' onclick='test()'>Delete</a>" +
-									"</li>");
-		$("#previousNotes").listview("refresh");
+		var name = document.getElementById("noteName").value;
+		var note = document.getElementById("note").value;
+		
+		if(name == "" && note != ""){
+			count++;
+			name = "Untitled Note " + count;
+		}
+		if(note == ""){
+			$("#emptyNote").popup("open");
+		}
+		else{
+			$("#previousNotes").append("<div data-role='collapsible'>" +
+											"<h3>" + name + "</h3>" +
+											"<p class='name'><strong>" + name + "</strong></p>" +
+											"<p class='content'>" + note + "</p>" +
+											"<div class='ui-grid-a'>" +
+												"<div class='ui-block-a'><a href='#' class='ui-btn edit'>Edit</a></div>" +
+												"<div class='ui-block-b'><a href='#' class='ui-btn delete'>Delete</a></div>" +
+											"</div>" +
+										"</div>");
+										
+			$("#previousNotes").collapsibleset("refresh");
+			
+			$(".delete").on("click", function(){
+				var item = $(this).parent("div").parent("div").parent("div").parent("div");
+				confirmAndDelete(item);
+			});
+			
+			$(".edit").on("click", function(){
+				var item = $(this).parent("div").parent("div").parent("div").parent("div");
+				edit(item);
+			});
+		}
 	});
 	
-	$("#cancel").click(function(){
+	$("#clear").click(function(){
 		document.getElementById("noteForm").reset();
 	});
 });
 
-function test() {
-	// Click delete split-button to remove list item
-	$( ".delete" ).on( "click", function() {
-		var listitem = $( this ).parent( "li" );
-		confirmAndDelete( listitem );
+function confirmAndDelete(item){
+	$("#confirm .name").remove();
+	item.find(".name").clone().insertAfter("#question");
+	
+	$("#confirm").popup("open");
+	
+	$("#confirm #yes").on("click", function(){
+		item.remove();
+		$("#previousNotes").collapsibleset("refresh");
 	});
 	
-    function confirmAndDelete( listitem, transition ) {
-        // Highlight the list item that will be removed
-        listitem.children( ".ui-btn" ).addClass( "ui-btn-active" );
-        // Inject topic in confirmation popup after removing any previous injected topics
-        $( "#confirm .topic" ).remove();
-        listitem.find( ".topic" ).clone().insertAfter( "#question" );
-        // Show the confirmation popup
-        $( "#confirm" ).popup( "open" );
-        // Proceed when the user confirms
-        $( "#confirm #yes" ).on( "click", function() {
-            // Remove with a transition
-            if ( transition ) {
-                listitem
-                    // Add the class for the transition direction
-                    .addClass( transition )
-                    // When the transition is done...
-                    .on( "webkitTransitionEnd transitionend otransitionend", function() {
-                        // ...the list item will be removed
-                        listitem.remove();
-                        // ...the list will be refreshed and the temporary class for border styling removed
-                        $( "#previousNotes" ).listview( "refresh" ).find( ".border-bottom" ).removeClass( "border-bottom" );
-                    })
-                    // During the transition the previous button gets bottom border
-                    .prev( "li" ).children( "a" ).addClass( "border-bottom" )
-                    // Remove the highlight
-                    .end().end().children( ".ui-btn" ).removeClass( "ui-btn-active" );
-            }
-            // If it's not a touch device or the CSS transition isn't supported just remove the list item and refresh the list
-            else {
-                listitem.remove();
-                $( "#previousNotes" ).listview( "refresh" );
-            }
-        });
-        // Remove active state and unbind when the cancel button is clicked
-        $( "#confirm #cancel" ).on( "click", function() {
-            listitem.removeClass( "ui-btn-active" );
-            $( "#confirm #yes" ).off();
-        });
-    }
+	$("#confirm #cancel").on("click", function(){
+		$("#confirm #yes").off();
+	});
+}
+
+function edit(item){
+	
 }
