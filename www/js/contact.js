b5624116aa2cc8a3d3d3405a8dbcf48a7d270b8a
Home <Home>
2016-03-31 08:58:14 -0600

    job coach info

--- www/js/contact.js
+++ www/js/contact.js
@@ -1,3 +1,27 @@
 jQuery(document).ready(function () {
-
+	var uri = 'http://pjcdbrebuild.gear.host/api/';
+    var loginToken = window.localStorage.getItem("token");
+	$.getJSON(uri + "JobCoach",
+		{token: loginToken},
+        function (data) {
+          // On success, 'data' contains JobCoach Info.
+          localStorage.setItem('jobcoach', JSON.stringify(data));
+            console.log(localStorage.getItem('jobcoach'));
+			displayJobCoachInfo();
+        }
+		).error(function() {
+			console.log("jobcoach not set");  
+		});
 });
+
+
+function displayJobCoachInfo() {
+        var jobcoach = JSON.parse(localStorage.getItem('jobcoach'));
+
+        $.each(jobcoach, function (key, item) {
+			console.log(item);
+			$('#firstName').text(item.userName);
+            
+        });
+
+    }
